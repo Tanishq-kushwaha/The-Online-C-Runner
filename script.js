b26d41cf9e7fff7b443cf5a6185addbc77152454
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-07-23 19:25:37 +0530

    Update terminal output display and enhance terminal styles for better visibility

--- script.js
+++ script.js
@@ -5,20 +5,12 @@ const terminal = document.getElementById("terminal");
 
 const heading = document.querySelector("#editor");
 
+const textArea = document.querySelector("textarea");
+
+const terminalText = document.querySelector("p");
+
 // When the button is clicked
 runBtn.addEventListener("click", async () => {
-
-    // Adds/removes the terminal's show class
-    terminal.classList.toggle("show");
-
-    if (terminal.classList.contains("show")) {
-        runBtn.innerText = "✖ Close";
-        runBtn.classList.remove("btn-success");
-        runBtn.classList.add("btn-danger");
-    }else{
-        runBtn.innerText = "▶ Run Code";
-        runBtn.classList.remove("btn-danger");
-        runBtn.classList.add("btn-success");
-
-    }
+   let code = textArea.value;
+   terminalText.innerText = code;
 });
